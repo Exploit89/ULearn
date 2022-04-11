27e6547fa15bcb587097c3d0a6fa8418a07ed57f
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-04-11 23:05:55 +1000

    PC_Home Ulearn commit

---
+++ Methods/Methods.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Methods
+{
+	class Methods
+	{
+
+		// Это метод, возвращающий значение типа int, принимающий два аргумента типа double.
+		// Его можно называть функцией, но это название не очень распространено.
+		// Сигнатура метода - это совокупность имени метода и последовательности типов аргументов
+		static int DivideAndRound(double a, double b)
+		{
+			// return указывает, какое значение будет возвращено
+			return (int)Math.Round(a / b);
+		}
+
+		// Это метод, не возвращающий значения. Вместо типа возвращаемого значения стоит void
+		static void WriteNumber(int number)
+		{
+			Console.WriteLine(number);
+
+			// return указывается без значения, и его следует опускать, когда это возможно
+			return;
+		}
+
+		static void WriteNumber(int number, int anotherNumber)
+		{
+			Console.WriteLine(number);
+			Console.WriteLine(anotherNumber);
+		}
+
+		static void Main()
+		{
+			WriteNumber(DivideAndRound(10.5, 2.1));
+			Console.WriteLine(Math.Ceiling(10.01));
+			Print(GetSquare(42));
+			Console.WriteLine("\n");
+			Console.WriteLine(GetLastHalf("I love CSharp!"));
+			Console.WriteLine(GetLastHalf("1234567890"));
+			Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+		}
+
+		public static int GetSquare(int number)
+		{
+			return number * number;
+		}
+		public static void Print(int numberValue)
+		{
+			Console.WriteLine(numberValue);
+		}
+
+		public static string GetLastHalf(string text)
+        {
+			int halfText = text.Length / 2;
+			string c = text.Substring(halfText, halfText);
+			return c.Replace(" ", "");
+        }
+	}
+}
---
+++ Variables/Variables.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+	class Program
+	{
+		static string globalVariable = "Global variable";
+
+		static void MethodA()
+		{
+			if (globalVariable == "")
+			{
+				string temporalVariable = "Temporal variable";
+				Console.WriteLine(temporalVariable);
+			}
+
+			string localVariable = "Local variable";
+
+			// Так можно — эта переменная используется в той же области, где и объявлена:
+			Console.WriteLine(localVariable);
+
+			// Так нельзя — temporalVariable определена только внутри блока if:
+			// Console.WriteLine(temporalVariable); 
+		}
+
+		static void MethodB()
+		{
+			// Console.WriteLine(localVariable); //Нельзя — переменная определена в другом методе.
+			Console.WriteLine(globalVariable); //Можно — это глобальная переменная
+
+		}
+	}
+
+	class Sample1
+	{
+		static int F()
+		{
+			i = 1;
+			return i;
+		}
+
+		static int i = 0;
+	}
+
+	class Sample2
+	{
+		static string who = "class";
+
+		static void F()
+		{
+			string who = "F";
+		}
+
+		static string G()
+		{
+			F();
+			return who;
+		}
+
+		static string H()
+		{
+			string who = "H";
+			F();
+			return who;
+		}
+	}
+
+	class Sample3
+	{
+		static string who = "class";
+
+		static string Mixed()
+		{
+			Console.Write(who + " ");
+			string who = "Mixed";
+			Console.Write(who);
+			return who;
+		}
+
+		public static void Main()
+		{
+			Console.WriteLine(Mixed());
+		}
+	}
+	class Sample4
+	{
+		void M1()
+		{
+			int i = 0;
+			{
+				int i = 1;
+			}
+		}
+		void M2(int i)
+		{
+			int i = 0;
+		}
+		void M3()
+		{
+			// Фигурными скобками можно группировать операторы (правда почти никогда этого делать не стоит)
+			// и каждая пара фигурных скобок создает свою область видимости.
+			{
+				int i = 0;
+			}
+			{
+				int i = 1;
+			}
+		}
+	}
+}
